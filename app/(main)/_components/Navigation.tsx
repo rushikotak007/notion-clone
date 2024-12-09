7eea8047d266c979a018d4ace5afd64e459d390b
rushikotak007 <94125233+rushikotak007@users.noreply.github.com>
2024-12-09 12:36:57 +0530

    updated Navigation.tsx

--- app/(main)/_components/Navigation.tsx
+++ app/(main)/_components/Navigation.tsx
@@ -1,24 +1,61 @@
 import React from "react";
+import { useState, useRef, ElementRef } from "react";
+import { useMediaQuery } from "usehooks-ts";
 
-import { ChevronsLeft } from "lucide-react";
+import { ChevronsLeft, MenuIcon } from "lucide-react";
+import { cn } from "@/lib/utils";
+import { usePathname } from "next/navigation";
 
 const Navigation = () => {
+  const pathname = usePathname();
+  const isMobile = useMediaQuery("(max-width: 768px)");
+
+  const isResizingRef = useRef(false);
+  const sidebarRef = useRef<ElementRef<"aside">>(null);
+  const navbarRef = useRef<ElementRef<"div">>(null);
+  const [isResetting, setIsResetting] = useState(false);
+  const [isCollapsed, setIsCollapsed] = useState(isMobile);
+
   return (
-    <aside className="group/sidebar h-full bg-secondary overflow-y-auto relative flex w-60 flex-col z-[99999]">
-      <div
-        role="button"
-        className="h-6 w-6 text-muted-foreground rounded-sm hover:bg-neutral-300 dark:hover:bg-neutral-600 absolute top-3 right-2 opacity-0 group-hover/sidebar:opacity-100 transition"
+    <>
+      <aside
+        ref={sidebarRef}
+        className={cn(
+          "group/sidebar h-full bg-secondary overflow-y-auto relative flex w-60 flex-col z-[99999]",
+          isResetting && "transition-all duration-300 ease-in-out",
+          isMobile && "w-0"
+        )}
       >
-        <ChevronsLeft className="h-6 w-6" />
+        <div
+          role="button"
+          className={cn(
+            "h-6 w-6 text-muted-foreground rounded-sm hover:bg-neutral-300 dark:hover:bg-neutral-600 absolute top-3 right-2 opacity-0 group-hover/sidebar:opacity-100 transition",
+            isMobile && "opacity-100"
+          )}
+        >
+          <ChevronsLeft className="h-6 w-6" />
+        </div>
+        <div>
+          <p>Action items</p>
+        </div>
+        <div className="mt-4">
+          <p>Documents</p>
+        </div>
+        <div className="opacity-0 group-hover/sidebar:opacity-100 transition cursor-ew-resize absolute h-full w-1 bg-primary/10 right-0 top-0" />
+      </aside>
+      <div
+        ref={navbarRef}
+        className={cn(
+          "absolute top-0 z-[99999] left-60 w=[calc(100% - 240px)]",
+          isResetting && "transition-all duration-300 ease-in-out",
+          isMobile && "w-full left-0"
+        )}
+      >
+        <nav>
+          {isCollapsed && <MenuIcon role="button" className="h-6 w-6 text-muted-foreground" />}
+        </nav>
       </div>
-      <div>
-        <p>Action items</p>
-      </div>
-      <div className="mt-4">
-        <p>Documents</p>
-      </div>
-      <div className="opacity-0 group-hover/sidebar: opacity-100 transition cursor-ew-resize absolute h-full w-1 bg-primary/10 right-0 top-0" />
-    </aside>
+    </>
   );
 };
 
