38124fecc41a658d2859c5fc9ec4ca004e4f7fdb
rushikotak007 <94125233+rushikotak007@users.noreply.github.com>
2024-12-09 23:33:57 +0530

    Completed Navigation Bar with animations

--- app/(main)/_components/Navigation.tsx
+++ app/(main)/_components/Navigation.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useState, useRef, ElementRef } from "react";
+import { useState, useEffect, useRef, ElementRef } from "react";
 import { useMediaQuery } from "usehooks-ts";
 
 import { ChevronsLeft, MenuIcon } from "lucide-react";
@@ -16,6 +16,83 @@ const Navigation = () => {
   const [isResetting, setIsResetting] = useState(false);
   const [isCollapsed, setIsCollapsed] = useState(isMobile);
 
+  const handleMousedown = (
+    event: React.MouseEvent<HTMLDivElement, MouseEvent>
+  ) => {
+    event.preventDefault();
+    event.stopPropagation();
+
+    isResizingRef.current = true;
+    document.addEventListener("mousemove", handleMousemove);
+    document.addEventListener("mouseup", handleMouseup);
+  };
+
+  const handleMousemove = (event: MouseEvent) => {
+    if (!isResizingRef.current) return;
+    let newWidth = event.clientX;
+    if (newWidth < 240) newWidth = 240;
+    if (newWidth > 480) newWidth = 480;
+
+    if (sidebarRef.current && navbarRef.current) {
+      sidebarRef.current.style.width = `${newWidth}px`;
+      navbarRef.current.style.setProperty("left", `${newWidth}px`);
+      navbarRef.current.style.setProperty(
+        "width",
+        `calc(100% - ${newWidth}px)`
+      );
+    }
+  };
+
+  const handleMouseup = () => {
+    isResizingRef.current = false;
+    document.removeEventListener("mousemove", handleMousemove);
+    document.removeEventListener("mouseup", handleMouseup);
+  };
+
+  const resetWidth = () => {
+    if (sidebarRef.current && navbarRef.current) {
+      setIsCollapsed(false);
+      setIsResetting(true);
+
+      sidebarRef.current.style.width = isMobile ? "100%" : "240px";
+      navbarRef.current.style.setProperty("left", isMobile ? "100%" : "240px");
+      navbarRef.current.style.setProperty(
+        "width",
+        isMobile ? "0" : "calc(100% - 240px)"
+      );
+      setTimeout(() => {
+        setIsResetting(false);
+      }, 300);
+    }
+  };
+
+  const collapse = () => {
+    if (sidebarRef.current && navbarRef.current) {
+      setIsCollapsed(true);
+      setIsResetting(true);
+
+      sidebarRef.current.style.width = "0";
+      navbarRef.current.style.setProperty("left", "0");
+      navbarRef.current.style.setProperty("width", "100%");
+      setTimeout(() => {
+        setIsResetting(false);
+      }, 300);
+    }
+  };
+
+  useEffect(() => {
+    if (isMobile) {
+      collapse();
+    } else {
+      resetWidth();
+    }
+  }, [isMobile]);
+
+  useEffect(() => {
+    if(isMobile) {
+      collapse();
+    }}, [pathname]);
+
   return (
     <>
       <aside
@@ -27,6 +104,7 @@ const Navigation = () => {
         )}
       >
         <div
+          onClick={collapse}
           role="button"
           className={cn(
             "h-6 w-6 text-muted-foreground rounded-sm hover:bg-neutral-300 dark:hover:bg-neutral-600 absolute top-3 right-2 opacity-0 group-hover/sidebar:opacity-100 transition",
@@ -41,7 +119,11 @@ const Navigation = () => {
         <div className="mt-4">
           <p>Documents</p>
         </div>
-        <div className="opacity-0 group-hover/sidebar:opacity-100 transition cursor-ew-resize absolute h-full w-1 bg-primary/10 right-0 top-0" />
+        <div
+          onMouseDown={handleMousedown}
+          onClick={resetWidth}
+          className="opacity-0 group-hover/sidebar:opacity-100 transition cursor-ew-resize absolute h-full w-1 bg-primary/10 right-0 top-0"
+        />
       </aside>
       <div
         ref={navbarRef}
@@ -51,8 +133,14 @@ const Navigation = () => {
           isMobile && "w-full left-0"
         )}
       >
-        <nav>
-          {isCollapsed && <MenuIcon role="button" className="h-6 w-6 text-muted-foreground" />}
+        <nav className="bg-transparent px-3 [y-2 w-full">
+          {isCollapsed && (
+            <MenuIcon
+              onClick={resetWidth}
+              role="button"
+              className="h-6 w-6 text-muted-foreground"
+            />
+          )}
         </nav>
       </div>
     </>
